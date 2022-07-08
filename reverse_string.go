b79ddfffadae92398f74f816027fba21e7c30fd6
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-07-08 17:58:20 +0600

    reverse string in go

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,19 @@
 package reverse_string
 
+//package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(ReverseString("Hello world!"))
+}
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	for i := len(input) - 1; i >= 0; i-- {
+		output += string(input[i])
+	}
+
 	return output
 }
